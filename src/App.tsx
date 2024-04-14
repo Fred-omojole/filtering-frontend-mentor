771a355bad43854a70a7d11e16731d545962b465
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-04-14 23:31:19 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -11,39 +11,44 @@ const App = () => {
         <>
           {/* <div className=""> */}
           <div
-            className="flex rounded-lg space-y-2  mt-4 bg-white p-5 relative"
+            className="flex rounded-lg  mt-4 bg-white p-5 relative"
             key={id}
           >
             <div>
               <Photo url={job.logo} desc={"image"} />
             </div>
 
-            <div className="flex items-center text-xs gap-3 ml-3 tracking-tight h-0">
-              <p className="color1 font-bold">{job.company}</p>
-              <span className="">
-                {job.new ? (
-                  <p className="text-white font-semibold bg-[hsl(180,29%,50%)] rounded-full px-1 py-1">
-                    NEW!
-                  </p>
-                ) : null}
-              </span>
-              <p>
-                {job.featured ? (
-                  <p className="text-white font-semibold bg-[hsl(180,14%,20%)] rounded-full px-1 py-1">
-                    FEATURED
-                  </p>
-                ) : null}
-              </p>
-            </div>
-            <div className="text-black mx-20  h-0 ">
-              {/* <div className=""> */}
-                {" "}
-                <h1>{job.position}</h1>
-                <div className="flex mt-3">
+            <div className="grid grid-cols-1  text-xs gap-3 ml-3 tracking-tight h-0  ">
+              <div className="flex items-center  gap-2">
+                <p className="color1 font-bold">{job.company}</p>
+                <p>
+                  {job.new ? (
+                    <p className="text-white font-semibold bg-[hsl(180,29%,50%)] rounded-full px-1 py-1">
+                      NEW!
+                    </p>
+                  ) : null}
+                </p>
+                <p>
+                  {job.featured ? (
+                    <p className="text-white font-semibold bg-[hsl(180,14%,20%)] rounded-full px-1 py-1">
+                      FEATURED
+                    </p>
+                  ) : null}
+                </p>
+              </div>
+
+              
+                {/* <div className=""> */}
+               
+                  <h1>{job.position}</h1>
+             
+                <div className="flex">
                   <p>{job.postedAt}</p>
                   <p>{job.contract}</p>
                   <p>{job.location}</p>
                 </div>
+              
+
               {/* </div> */}
             </div>
           </div>
