4abfdeeb6320a8a5b36c8ff631561cfe859d850c
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-04-15 00:45:34 +0100

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import Header from "./Header.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <main className="min-h-[screen] overflow-hidden ">
+    <main className="min-h-[screen] overflow-hidden">
       <Header />
       <div className="max-w-4xl mx-auto p-5">
         <App />
